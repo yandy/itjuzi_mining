ccf8bb1c81e4b32de17362a7cc6802ef63b0a7e9
Michael Ding <yandy.ding@gmail.com>
2014-12-08 18:51:54 +0800

    Init Project

---
+++ itjuzi_mining/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for itjuzi_mining project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'itjuzi_mining'
+
+SPIDER_MODULES = ['itjuzi_mining.spiders']
+NEWSPIDER_MODULE = 'itjuzi_mining.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'itjuzi_mining (+http://www.yourdomain.com)'
